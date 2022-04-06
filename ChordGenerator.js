28c42005d3c57ca47efb9548714de819b43b7b74
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-04-06 17:39:28 +0300

    Update ChordGenerator.js

--- ChordGenerator.js
+++ ChordGenerator.js
@@ -1,7 +1,9 @@
 const functions = [
-    [8, 6],  //Tonic => main tonic written as 8 instead of 1 for computational reasons
-    [4, 2],  //Subdominant
-    [7, 5],  //Dominant
+    [8, 6], //Tonic => main tonic written as 8 instead of 1 for computational reasons
+    [8, 60],
+    [4, 2], //Subdominant
+    [40, 20],
+    [5, 50], //Dominant  
 ];
 
 let progression = [];
@@ -10,16 +12,35 @@ let funcIndex = 0;
 let chordCounter = 0;
 
 while (true) {
+
+
     for (let chord of functions[funcIndex]) {
         //decides to include new chord in progression
         if (Math.round(Math.random()) == 1) {
-            //avoids duplicates and reversing functional logic from subsidiary to main chords
-            if (progression.length > 0 &&
+            //avoids duplicates
+            if (progression.length > 0 && //avoids C after Am
                 (progression[progression.length - 1] === chord || progression[progression.length - 1] === chord - 2)) {
                 continue;
             }
 
+            if (progression.length > 0 && //avoids C7 F after F and vice versa
+                (progression[progression.length - 1] === chord / 10 || progression[progression.length - 1] === chord * 10)) {
+                continue;
+            }
+
+            if (progression.length > 0 && //avoids C7 F after Dm???,  F after A7 Dm
+                (progression[progression.length - 1] === (chord / 10) - 2 || progression[progression.length - 1] / 10 === (chord - 2))) {
+                continue;
+            }
+
+            //avoids duplicates at penultimate index
             if (progression.length >= 2 && progression[progression.length - 2] === chord) {
+                continue;
+            }
+
+            //same but with applied dominant chords
+            if (progression.length >= 2 &&
+                (progression[progression.length - 2] === chord / 10 || progression[progression.length - 2] === chord * 10)) {
                 continue;
             }
 
@@ -35,7 +56,7 @@ while (true) {
         }
     }
 
-    if (chordCounter === 24) {
+    if (chordCounter >= 24) {
         break;
     }
 
@@ -49,14 +70,22 @@ while (true) {
 //maps progression to chords in particular key
 //could be modified to any major or minor tonality
 const tonalChords = {
-    //A minor
-    [8] : "Am",
-    [2] : "Bdim",
-    [3] : "C",
-    [4] : "Dm",
-    [5] : "E7",
-    [6] : "F",
-    [7] : "G",
+    //C major
+    [8]: "C",
+    [2]: "Dm",
+    //[3]: "Em",
+    [4]: "F",
+    [5]: "G",
+    [6]: "Am",
+    [7]: "Bdim",
+    //[80]: "G9 C",
+    [20]: "A7, Dm",
+    //[30]: "Bdim, Em",
+    [40]: "C7, F",
+    [50]: "D7, G",
+    [60]: "E7, Am",
+    //[70]: "F#9 Bdim",
+
 
     //F minor
     // [8]: "F min",
