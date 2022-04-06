5e1444b049cc3b344fabcb897bef36909e0453ff
kaloyan.ivanov <kaloyan.ivanov@mentormate.com>
2022-04-06 18:32:54 +0300

    prepared skeleton for diminutions version

--- ChordGenerator.js
+++ ChordGenerator.js
@@ -1,9 +1,7 @@
 const functions = [
-    [8, 6], //Tonic => main tonic written as 8 instead of 1 for computational reasons
-    [8, 60],
-    [4, 2], //Subdominant
-    [40, 20],
-    [5, 50], //Dominant  
+    [8, 6], //Tonic => main tonic written as 8 instead of 1 for computational reasons   
+    [4, 2], //Subdominant    
+    [5], //Dominant  
 ];
 
 let progression = [];
@@ -21,24 +19,8 @@ while (true) {
                 continue;
             }
 
-            if (progression.length > 0 && //avoids C7 F after F and vice versa
-                (progression[progression.length - 1] === chord / 10 || progression[progression.length - 1] === chord * 10)) {
-                continue;
-            }
-
-            if (progression.length > 0 && //avoids C7 F after Dm???,  F after A7 Dm
-                (progression[progression.length - 1] === (chord / 10) - 2 || progression[progression.length - 1] / 10 === (chord - 2))) {
-                continue;
-            }
-
             //avoids duplicates at penultimate index
             if (progression.length >= 2 && progression[progression.length - 2] === chord) {
-                continue;
-            }
-
-            //same but with applied dominant chords
-            if (progression.length >= 2 &&
-                (progression[progression.length - 2] === chord / 10 || progression[progression.length - 2] === chord * 10)) {
                 continue;
             }
 
@@ -76,14 +58,8 @@ const tonalChords = {
     [5]: "G",
     [6]: "Am",
     //[7]: "Bdim",
-    //[80]: "G9 C",
-    [20]: "A7, Dm",
-    //[30]: "Bdim, Em",
-    [40]: "C7, F",
-    [50]: "D7, G",
-    [60]: "E7, Am",
-    //[70]: "F#9 Bdim",
-
+    [50]: "Gsus",
+    [80]: "C/G"
 
     //F minor
     // [8]: "F min",
@@ -100,6 +76,19 @@ let result = '';
 for (let chord of progression) {
     result += `${tonalChords[chord]}, `;
 }
+
+//creates final authentic cadence
+if (progression[progression.length - 1] !== 5) {
+
+    if (Math.round(Math.random()) === 0) {
+        result += `${tonalChords[50]}, `;
+    } else {
+        result += `${tonalChords[80]}, `;
+    }
+
+    result += `${tonalChords[5]}, `;
+}
+result += `${tonalChords[8]}`;
 
 //prints progression
 console.log(result.toString().trim());
