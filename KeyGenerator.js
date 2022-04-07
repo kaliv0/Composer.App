341ee122fd42c975c23e19cc8ef79b149b21ba80
kaloyan.ivanov <kaloyan.ivanov@mentormate.com>
2022-04-07 21:20:38 +0300

    added helper function for key signature

--- KeyGenerator.js
+++ KeyGenerator.js
@@ -1,38 +1,54 @@
-const pitches = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-let newKey = [];
-let counter = 0;
-let pitchIndex = 5;
+function generateKey(tonality) {
+    const pitches = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    const tonic = tonality.substring(2);
+    let newKey = [];
+    let counter = 0;
+    let firstDegree = tonic.charAt(0);
+    let pitchIndex = pitches.indexOf(firstDegree);
 
-//populates scale degrees
-while (counter <= 7) {
-    newKey.push(pitches[pitchIndex]);
+    //populates scale degrees
+    while (counter < 7) {
+        newKey.push(pitches[pitchIndex]);
 
-    if (pitchIndex < pitches.length - 1) {
-        pitchIndex++;
-    } else {
-        pitchIndex = 0;
+        if (pitchIndex < pitches.length - 1) {
+            pitchIndex++;
+        } else {
+            pitchIndex = 0;
+        }
+
+        counter++;
     }
 
-    counter++;
+    if (tonality.contains('minor')) {
+        //todo
+    }
+
+    newKey = addSharps(newKey);
+    console.log(newKey.join(' '));
+
+    //helper function => adds key signature
+    function addSharps(scale) {
+
+        const sharps = ['F', 'C', 'G', 'D', 'A', 'E', 'B'];
+        const majorCircleOfFifts = {
+            'C': 0,
+            'G': 1,
+            'D': 2,
+            'A': 3,
+            'E': 4,
+            'B': 5,
+            'F#': 6,
+            'C#': 7,
+        }
+
+        const sharpCount = majorCircleOfFifts[tonic];//todo
+        for (let i = 0; i < sharpCount; i++) {
+            let degreeIndex = newKey.indexOf(sharps[i]);
+            newKey[degreeIndex] += '#';
+        }
+
+        return scale;
+    }
 }
 
-//adds key signature
-const sharps = ['F', 'C', 'G', 'D', 'A', 'E', 'B'];
-const majorCircleOfFifts = {
-    'C': 0,
-    'G': 1,
-    'D': 2,
-    'A': 3,
-    'E': 4,
-    'B': 5,
-    // 'F': 0,
-    // 'C': 0,
-}
-
-const sharpCount = circleOfFifts[newKey[0]];//todo
-for (let i = 0; i < sharpCount; i++) {
-    let degreeIndex = newKey.indexOf(sharps[i]);
-    newKey[degreeIndex] += '#';
-}
-
-console.log(newKey.join(' '));
+generateKey('G')
