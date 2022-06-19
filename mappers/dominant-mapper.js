2c265df7da51c7a05e6927644c5effa217835fea
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-06-19 12:43:03 +0300

    unified approach for major and minor mode

--- mappers/dominant-mapper.js
+++ mappers/dominant-mapper.js
@@ -1,77 +1,58 @@
 const alterator = require('../alterators/note-alterator');
 
 function translate(scale, root, mode) {
-    /* TODO => refactor by setting different variables and boolenas depending on particular mode
-            => otherwise the algorithm is the same */
+    let raiseThirdIndeces;
+    let lowerSeventhIndeces;
+    let rootIndex;
+    let scaleIndex;
+    let raisedFiveIndex;
+    let currNote;
+    let fullChord = [];
+
     if (mode === 'major') {
-        const raiseThirdIndeces = [1, 2, 5, 6];
-        const lowerSeventhIndeces = [0, 3];
-        let rootIndex = scale.indexOf(root);
-        let scaleIndex = rootIndex;
-        let fullChord = [];
-        let currNote;
-
-        //reads other notes above root and adds accidentals where necessary
-        for (let j = 0; j < 4; j++) {
-            currNote = scale[scaleIndex];
-
-            if (j === 1 && raiseThirdIndeces.includes(rootIndex)) {
-                currNote = alterator.raiseNote(currNote);
-            }
-
-            if (j === 2 && rootIndex === 6) {
-                currNote = alterator.raiseNote(currNote);
-            }
-
-            if (j === 3 && lowerSeventhIndeces.includes(rootIndex)) {
-                currNote = alterator.lowerNote(currNote);
-            }
-
-            fullChord.push(currNote);
-            //keeps index within octave boundaries
-            if (scaleIndex + 2 >= 7) {
-                scaleIndex -= 5;
-            } else {
-                scaleIndex += 2;
-            }
-        }
-        return fullChord;
+        raiseThirdIndeces = [1, 2, 5, 6];
+        lowerSeventhIndeces = [0, 3];
+        rootIndex = scale.indexOf(root);
+        scaleIndex = rootIndex;
+        raisedFiveIndex = 6;
+        fullChord = [];
+        currNote;
+    } else {
+        //sets variables for minor mode
+        raiseThirdIndeces = [0, 1, 3, 4];
+        lowerSeventhIndeces = [2, 5];
+        rootIndex = scale.indexOf(root);
+        scaleIndex = rootIndex;
+        raisedFiveIndex = 1;
+        fullChord = [];
+        currNote;
     }
 
-    if (mode === 'minor') {
-        const raiseThirdIndeces = [0, 1, 3, 4];
-        const lowerSeventhIndeces = [2, 5];
-        let rootIndex = scale.indexOf(root);
-        let scaleIndex = rootIndex;
-        let fullChord = [];
-        let currNote;
+    //reads other notes above root and adds accidentals where necessary
+    for (let j = 0; j < 4; j++) {
+        currNote = scale[scaleIndex];
 
-        //reads other notes above root and adds accidentals where necessary
-        for (let j = 0; j < 4; j++) {
-            currNote = scale[scaleIndex];
-
-            if (j === 1 && raiseThirdIndeces.includes(rootIndex)) {
-                currNote = alterator.raiseNote(currNote);
-            }
-
-            if (j === 2 && rootIndex === 1) {
-                currNote = alterator.raiseNote(currNote);
-            }
-
-            if (j === 3 && lowerSeventhIndeces.includes(rootIndex)) {
-                currNote = alterator.lowerNote(currNote);
-            }
-
-            fullChord.push(currNote);
-            //keeps index within octave boundaries
-            if (scaleIndex + 2 >= 7) {
-                scaleIndex -= 5;
-            } else {
-                scaleIndex += 2;
-            }
+        if (j === 1 && raiseThirdIndeces.includes(rootIndex)) {
+            currNote = alterator.raiseNote(currNote);
         }
-        return fullChord;
+
+        if (j === 2 && rootIndex === raisedFiveIndex) {
+            currNote = alterator.raiseNote(currNote);
+        }
+
+        if (j === 3 && lowerSeventhIndeces.includes(rootIndex)) {
+            currNote = alterator.lowerNote(currNote);
+        }
+
+        fullChord.push(currNote);
+        //keeps index within octave boundaries
+        if (scaleIndex + 2 >= 7) {
+            scaleIndex -= 5;
+        } else {
+            scaleIndex += 2;
+        }
     }
+    return fullChord;
 }
 
 module.exports = { translate };
