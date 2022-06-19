245b114a3c6db280806fa325203ccca27baedd72
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-06-19 12:49:29 +0300

    fixed variable initalization

--- mappers/dominant-mapper.js
+++ mappers/dominant-mapper.js
@@ -3,30 +3,23 @@ const alterator = require('../alterators/note-alterator');
 function translate(scale, root, mode) {
     let raiseThirdIndeces;
     let lowerSeventhIndeces;
-    let rootIndex;
-    let scaleIndex;
     let raisedFiveIndex;
-    let currNote;
-    let fullChord = [];
 
     if (mode === 'major') {
         raiseThirdIndeces = [1, 2, 5, 6];
         lowerSeventhIndeces = [0, 3];
-        rootIndex = scale.indexOf(root);
-        scaleIndex = rootIndex;
         raisedFiveIndex = 6;
-        fullChord = [];
-        currNote;
     } else {
         //sets variables for minor mode
         raiseThirdIndeces = [0, 1, 3, 4];
         lowerSeventhIndeces = [2, 5];
-        rootIndex = scale.indexOf(root);
-        scaleIndex = rootIndex;
         raisedFiveIndex = 1;
-        fullChord = [];
-        currNote;
     }
+
+    let rootIndex = scale.indexOf(root);
+    let scaleIndex = rootIndex;
+    let fullChord = [];
+    let currNote;
 
     //reads other notes above root and adds accidentals where necessary
     for (let j = 0; j < 4; j++) {
