d10819cb30b976f38d20646e5a165ab6781bb6d8
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-06-20 11:28:58 +0300

    refactored as higher-order function

--- mappers/tonal-mapper.js
+++ mappers/tonal-mapper.js
@@ -1,12 +1,10 @@
 //maps progression to chords in particular key
 //could be modified to any major or minor tonality
 function materialize(progression, tonalChords) {
-    let result = progression.reduce((acc, val) => {
+    return progression.reduce((acc, val) => {
         acc.push(tonalChords[val]);
         return acc;
     }, []);
-
-    return result;
 }
 
 module.exports = { materialize };
