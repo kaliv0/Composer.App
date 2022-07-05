8ec78a186ae29d403b755ca1530977313ac3b86f
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-07-05 09:30:48 +0300

    renamed reduce variables

--- mappers/tonal-mapper.js
+++ mappers/tonal-mapper.js
@@ -1,9 +1,9 @@
 //maps progression to chords in particular key
 //could be modified to any major or minor tonality
 function materialize(progression, tonalChords) {
-    return progression.reduce((acc, val) => {
-        acc.push(tonalChords[val]);
-        return acc;
+    return progression.reduce((materializedProgression, chordIndex) => {
+        materializedProgression.push(tonalChords[chordIndex]);
+        return materializedProgression;
     }, []);
 }
 
